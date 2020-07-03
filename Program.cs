3624efb51ccafa24beab9f6bfecaebf84b5c7cc9
Xavier Fischer <xavier@fischer.ninja>
2020-07-03 09:36:40 +0200

    intial checkin

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Utilities;
+using NetTopologySuite.IO;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Useful visualization/editing tools :
+                // WKT: http://arthur-e.github.io/Wicket/sandbox-gmaps3.html
+                // GeoJSON : http://geojson.io/
+
+
+                // Read GeoJSON
+                const string json = "{\"type\": \"Polygon\",\"coordinates\": [[[-111.70898437499999,38.71980474264237],[-111.3134765625,37.54457732085582],[-109.77539062499999,37.89219554724437],[-110.390625,39.774769485295465],[-111.70898437499999,38.71980474264237]]]}}";                
+                Geometry result = new GeoJsonReader().Read<Geometry>(json);
+                Console.WriteLine("Read geometry: " + result.ToString());
+
+                // Compute envelope
+                Geometry envelopeFromDB = result.Envelope;
+                Console.WriteLine("Envelope (Lat/Lon): " + envelopeFromDB.ToString());
+
+                // Build a polygon from scratch
+                Polygon polygon = new Polygon(new LinearRing(new [] { 
+                    new Coordinate(-114, 39.5),
+                    new Coordinate(-114, 36.25),
+                    new Coordinate(-110, 36.25),
+                    new Coordinate(-110, 39.5),
+                    new Coordinate(-114, 39.5) }));
+                Console.WriteLine("Manual Envelope (Lat/Lon): " + polygon.ToString());
+
+
+                // Intersection test
+                bool intersects = envelopeFromDB.Intersects(polygon);
+                Console.WriteLine("Intersection result: " + polygon.ToString());
+
+                // Reprojection functions. May help
+                //
+                //var envelope3857 = ProjectionSettings.Transform(envelopeFromDB, ProjectionSettings.Instance.WGS84ToWebMercatorTransformer);
+                //Console.WriteLine("Envelope (WebMercator) : " + envelope3857.ToString());
+                //var envelope4326 = ProjectionSettings.Transform(envelope3857, ProjectionSettings.Instance.WebMercatorToWGS84Transformer);
+                //Console.WriteLine("Envelope (Back to Lat/Lon) : " + envelope4326.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+      
+
+       
+    }
+}
---
+++ ProjectionSettings.cs
@@ -0,0 +1,94 @@
+﻿using NetTopologySuite.Geometries;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestSample
+{
+    public class ProjectionSettings
+    {
+        private static ProjectionSettings instance;
+
+        private ProjectionSettings() { }
+
+
+        public CoordinateSystem WGS84CoordinateSystem { get; private set; }
+        public CoordinateSystem UTMCoordinateSystem { get; private set; }
+        public CoordinateSystem WebMercatorCoordinateSystem { get; private set; }
+
+        public MathTransform WGS84ToUTMTransformer { get; private set; }
+        public MathTransform UTMToWGS84Transformer { get; private set; }
+
+        public MathTransform WGS84ToWebMercatorTransformer { get; private set; }
+        public MathTransform WebMercatorToWGS84Transformer { get; private set; }
+
+        public GeometryFactory WGS84GeometryFactory { get; private set; }
+        public GeometryFactory UTMGeometryFactory { get; private set; }
+        public GeometryFactory WebMercatorGeometryFactory { get; private set; }
+
+
+        public static ProjectionSettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ProjectionSettings();
+                    instance.Init();
+                }
+                return instance;
+            }
+        }
+
+        private void Init()
+        {
+            CoordinateSystem wgs84CoordinateSystem = GeographicCoordinateSystem.WGS84;
+            CoordinateSystem webMercatorCoordinateSystem = ProjectedCoordinateSystem.WebMercator;
+
+
+            this.WGS84CoordinateSystem = wgs84CoordinateSystem;
+            this.WebMercatorCoordinateSystem = webMercatorCoordinateSystem;
+
+            this.WGS84GeometryFactory = new GeometryFactory(new PrecisionModel(), (int)wgs84CoordinateSystem.AuthorityCode);
+            this.WebMercatorGeometryFactory = new GeometryFactory(new PrecisionModel(), (int)webMercatorCoordinateSystem.AuthorityCode);
+
+            var trfWeb = new CoordinateTransformationFactory();
+            this.WGS84ToWebMercatorTransformer = trfWeb.CreateFromCoordinateSystems(wgs84CoordinateSystem, webMercatorCoordinateSystem).MathTransform;
+            this.WebMercatorToWGS84Transformer = trfWeb.CreateFromCoordinateSystems(webMercatorCoordinateSystem, wgs84CoordinateSystem).MathTransform;
+        }
+
+        public static Geometry Transform(Geometry geom, MathTransform transform)
+        {
+            geom = geom.Copy();
+            geom.Apply(new MTF(transform));
+            return geom;
+        }
+
+
+    }
+
+    sealed class MTF : ICoordinateSequenceFilter
+    {
+        private readonly MathTransform _mathTransform;
+
+        public MTF(MathTransform mathTransform) => _mathTransform = mathTransform;
+
+        public bool Done => false;
+        public bool GeometryChanged => true;
+        public void Filter(CoordinateSequence seq, int i)
+        {
+            double x = seq.GetX(i);
+            double y = seq.GetY(i);
+            double z = seq.GetZ(i);
+            _mathTransform.Transform(ref x, ref y, ref z);
+            seq.SetX(i, x);
+            seq.SetY(i, y);
+            seq.SetZ(i, z);
+        }
+    }
+}
+
